34d1b5e98da56505a78d4f869f53d951e8de6386
gijinBang <rlwlsdnlwjem2@gmail.com>
2018-12-07 11:05:55 +0900

    little clean up for upload;

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,30 +7,30 @@ async function deploy(deployer) {
     _gas = 3000000
     _gasPrice = 1000
     if (args[3] == 'all') {
-        var _name='Metadium';
-        var _symbol='META';
-        
+        var _name = 'Metadium';
+        var _symbol = 'META';
+
         //proxy create metaID instead user for now. Because users do not have enough fee.
         var proxy1 = '0x084f8293F1b047D3A217025B24cd7b5aCe8fC657'; //node3 account[1]
 
-        return deployer.deploy(MetadiumNameService,{gas:_gas, gasPrice:_gasPrice}).then((mns)=>{
-            return deployer.deploy(MetaID, _name, _symbol,{gas:_gas, gasPrice:_gasPrice}).then((mid)=>{
-                return deployer.deploy(MetadiumIdentityManager,{gas:_gas, gasPrice:_gasPrice}).then(async function initialSetup(mim){
-                    
+        return deployer.deploy(MetadiumNameService, { gas: _gas, gasPrice: _gasPrice }).then((mns) => {
+            return deployer.deploy(MetaID, _name, _symbol, { gas: _gas, gasPrice: _gasPrice }).then((mid) => {
+                return deployer.deploy(MetadiumIdentityManager, { gas: _gas, gasPrice: _gasPrice }).then(async function initialSetup(mim) {
+
                     //mns: name, permission setup
-                    await mns.setContractDomain("MetaID", mid.address,{gas:_gas, gasPrice:_gasPrice})
-                    await mns.setContractDomain("MetadiumIdentityManager", mim.address,{gas:_gas, gasPrice:_gasPrice})
-                    await mns.setPermission("MetadiumIdentityManager", proxy1, "true",{gas:_gas, gasPrice:_gasPrice})
-                    await mns.setPermission("MetaID", mim.address, "true",{gas:_gas, gasPrice:_gasPrice})
-                    await mim.setMetadiumNameServiceAddress(mns.address,{gas:_gas, gasPrice:_gasPrice})
-                    await mid.setMetadiumNameServiceAddress(mns.address,{gas:_gas, gasPrice:_gasPrice})
-                    
+                    await mns.setContractDomain("MetaID", mid.address, { gas: _gas, gasPrice: _gasPrice })
+                    await mns.setContractDomain("MetadiumIdentityManager", mim.address, { gas: _gas, gasPrice: _gasPrice })
+                    await mns.setPermission("MetadiumIdentityManager", proxy1, "true", { gas: _gas, gasPrice: _gasPrice })
+                    await mns.setPermission("MetaID", mim.address, "true", { gas: _gas, gasPrice: _gasPrice })
+                    await mim.setMetadiumNameServiceAddress(mns.address, { gas: _gas, gasPrice: _gasPrice })
+                    await mid.setMetadiumNameServiceAddress(mns.address, { gas: _gas, gasPrice: _gasPrice })
+
                 })
             })
         })
-        
 
-        
+
+
     }
 }
 module.exports = deploy
